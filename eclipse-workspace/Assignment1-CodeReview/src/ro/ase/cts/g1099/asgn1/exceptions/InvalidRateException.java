c066e5aab98a5cb2664a8c817149a6b22389acb1
ANDREEA NEDELCU <NEDELCU1ANDREEA19@stud.ase.ro>
2022-04-04 13:30:37 +0300

    Exceptions

---
+++ eclipse-workspace/Assignment1-CodeReview/src/ro/ase/cts/g1099/asgn1/exceptions/InvalidRateException.java
@@ -0,0 +1,5 @@
+package ro.ase.cts.g1099.asgn1.exceptions;
+
+public class InvalidRateException extends RuntimeException {
+
+}
---
+++ eclipse-workspace/Assignment1-CodeReview/src/ro/ase/cts/g1099/asgn1/stage7/AccountType.java
@@ -0,0 +1,5 @@
+package ro.ase.cts.g1099.asgn1.stage7;
+
+public enum AccountType {
+	STANDARD,BUDGET,PREMIUM,SUPER_PREMIUM;
+}
