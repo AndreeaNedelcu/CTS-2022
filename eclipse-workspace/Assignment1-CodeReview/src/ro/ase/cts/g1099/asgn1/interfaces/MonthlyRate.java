728284f338d213e13e4779274060ad5f824f0b4d
ANDREEA NEDELCU <NEDELCU1ANDREEA19@stud.ase.ro>
2022-04-04 20:36:48 +0300

    Use interfaces

---
+++ eclipse-workspace/Assignment1-CodeReview/src/ro/ase/cts/g1099/asgn1/interfaces/MonthlyRate.java
@@ -0,0 +1,5 @@
+package ro.ase.cts.g1099.asgn1.interfaces;
+
+public interface MonthlyRate {
+	public double getMonthlyRate() ;
+}
---
+++ eclipse-workspace/Assignment1-CodeReview/src/ro/ase/cts/g1099/asgn1/stage9/AccountType.java
@@ -0,0 +1,5 @@
+package ro.ase.cts.g1099.asgn1.stage9;
+
+public enum AccountType {
+	STANDARD,BUDGET,PREMIUM,SUPER_PREMIUM;
+}
